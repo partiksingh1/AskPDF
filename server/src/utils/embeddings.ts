473224b5b96a5d13f35bfeb171816cd8c7718be1
partiksingh1 <partiksingh28@gmail.com>
2025-08-28 01:51:57 +0200

    config : embeddings

--- server/src/utils/embeddings.ts
+++ server/src/utils/embeddings.ts
@@ -1,5 +1,9 @@
-import { VertexAIEmbeddings } from "@langchain/google-vertexai";
-
-export const embeddings = new VertexAIEmbeddings({
-    model: "text-embedding-005"
+import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
+import { TaskType } from "@google/generative-ai";
+import dotenv from 'dotenv';
+dotenv.config();
+export const embeddings = new GoogleGenerativeAIEmbeddings({
+    model: "text-embedding-004", // 768 dimensions
+    taskType: TaskType.RETRIEVAL_DOCUMENT,
+    title: "langchain-vetors",
 });
